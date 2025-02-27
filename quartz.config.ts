335ae7ebd1cc19b2f5bbcd97d6678a4d1f9070a2
TatoNaranjo <naranjosa2004@gmail.com>
2025-02-27 09:25:50 -0500

    Quartz sync: Feb 27, 2025, 9:25 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Anonymous Pro",
+        header: "Montserrat",
         body: "Source Sans Pro",
         code: "JetBrains Mono",
       },
